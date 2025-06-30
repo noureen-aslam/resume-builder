3bd8ce83b0e80196a591a16833a430b0892bdb78
noureen-aslam <noureenaslam@gmail.com>
2025-06-30 13:32:53 +0530

    added template 2

--- script.js
+++ script.js
@@ -16,18 +16,39 @@ function generateResume() {
   const selectedDiv = document.getElementById(template);
   selectedDiv.style.display = 'block';
 
-  selectedDiv.innerHTML = `
-    <h1>${name}</h1>
-    <p><strong>Email:</strong> ${email} | <strong>Phone:</strong> ${phone}</p>
+   if (template === 'template1') {
+    selectedDiv.innerHTML = `
+      <h1>${name}</h1>
+      <p><strong>Email:</strong> ${email} | <strong>Phone:</strong> ${phone}</p>
+      <h2>Education</h2><p>${education}</p>
+      <h2>Experience</h2><p>${experience}</p>
+      <h2>Skills</h2><p>${skills}</p>
+      <h2>Projects</h2><p>${projects}</p>
+      <h2>Certificates</h2><p>${certificates}</p>
+    `;
+  }
 
-    <h2>Education</h2><p>${education}</p>
-    <h2>Experience</h2><p>${experience}</p>
-    <h2>Skills</h2><p>${skills}</p>
-    <h2>Projects</h2><p>${projects}</p>
-    <h2>Certificates</h2><p>${certificates}</p>
-  `;
+  if (template === 'template2') {
+    selectedDiv.innerHTML = `
+      <div class="template2-sidebar">
+        <h1>${name}</h1>
+        <p><strong>Email:</strong><br>${email}</p>
+        <p><strong>Phone:</strong><br>${phone}</p>
+        <p><strong>Skills:</strong><br>${skills}</p>
+        <p><strong>Certificates:</strong><br>${certificates}</p>
+      </div>
+      <div class="template2-main">
+        <h2>Education</h2><p>${education}</p>
+        <h2>Experience</h2><p>${experience}</p>
+        <h2>Projects</h2><p>${projects}</p>
+      </div>
+    `;
+  }
+  else {
+  selectedDiv.innerHTML = "<p>No preview available for this template.</p>";
 }
 
+}
 // Download function using jsPDF
 async function downloadPDF() {
   const { jsPDF } = window.jspdf;
